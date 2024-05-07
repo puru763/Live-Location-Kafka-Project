ba1598e1eaaedd15a684896844527e9ad5bdc8cc
Purvesh Krishnani <purvesh152001@gmail.com>
2024-05-08 00:53:10 +0530

    adding microservices

---
+++ deliveryperson/src/test/java/com/livelocationkafka/deliveryperson/DeliverypersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livelocationkafka.deliveryperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverypersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
